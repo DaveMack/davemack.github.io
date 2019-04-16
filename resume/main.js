f84b14d69071a42bc68cdad8fd48813eabdca4a3
dave <davebmackenzie@gmail.com>
2019-04-16 13:53:11 +1000

    Tidied up resume

--- resume/main.js
+++ resume/main.js
@@ -1,33 +1,6 @@
-
-/*
-  var elem = document.getElementById("bar"); 
-  var max = elem.innerText;
-  console.log(elem);
-  console.log(max);
-  var width = 1;
-  var id = setInterval(frame, 10);
-  function frame() {
-    if (width >= max) {
-      clearInterval(id);
-    } else {
-      width++; 
-      elem.style.width = width + '%'; 
-    }
-  }
-*/
-var bars = document.getElementsByClassName("bar");
-console.log(bars);
-for(let i = 0, length1 = bars.length; i < length1; i++){
-
-  var max = bars[i].innerText;
-  var width = 1;
-  var id = setInterval(frame, 10);
-  function frame() {
-    if (width >=  bars[i].innerText) {
-      clearInterval(id);
-    } else {
-      width++; 
-      bars[i].style.width = width + '%'; 
-    }
-  }
+let bars = document.getElementsByClassName("bar");
+console.log(bars.length);
+for(let i = 0; i < bars.length; i++){
+  console.log(bars[i]);
+  bars[i].style.width = bars[i].innerText; 
 }
